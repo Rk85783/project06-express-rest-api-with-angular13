edfa7e4dbc0a9f8b91cec4df6e374f5982109490
Rohit Kumar Mahor <rk85783@gmail.com>
2024-10-15 16:14:01 +0530

    03 - signup and login api completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,50 @@
 const userModel = require("../models/userModel");
 const messages = require("../utils/constantMessages");
-const { hashSync } = require("bcrypt");
+const { hashSync, compareSync } = require("bcrypt");
 
 module.exports = {
-  login: (req, res) => {
-    return res.status(200).json({
-      status: true,
-      message: "Ok"
-    });
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({
+          status: false,
+          message: messages.ALL_FIELDS_REQUIRED
+        });
+      }
+
+      const existsUser = await userModel.findOne({ email });
+      if (!existsUser) {
+        return res.status(400).json({
+          status: false,
+          message: messages.USER_NOT_FOUND
+        });
+      }
+
+      const isMatched = compareSync(password, existsUser.password);
+      if (!isMatched) {
+        return res.status(401).json({
+          status: false,
+          message: messages.INVALID_CREDENCIALS
+        });
+      }
+
+      const loggedUser = existsUser.toJSON();
+      delete loggedUser.password;
+
+      return res.status(200).json({
+        status: true,
+        message: messages.LOGIN_SUCCESS,
+        data: loggedUser
+      });
+    } catch (error) {
+      console.error("login(): catch(): error : ", error);
+      return res.status(200).json({
+        status: false,
+        message: messages.INTERNAL_SERVER_ERROR
+      });
+    }
   },
   signup: async (req, res) => {
     try {
