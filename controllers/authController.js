6857ace9d30d602b3f752c98779811b75a987313
Rohit Kumar Mahor <rk85783@gmail.com>
2024-10-15 15:58:35 +0530

    02 - product crud api - completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,43 @@
+const userModel = require("../models/userModel");
+const messages = require("../utils/constantMessages");
+const { hashSync } = require("bcrypt");
+
 module.exports = {
-  login: (req, res) => { },
-  signup: (req, res) => { }
+  login: (req, res) => {
+    return res.status(200).json({
+      status: true,
+      message: "Ok"
+    });
+  },
+  signup: async (req, res) => {
+    try {
+      const { firstName, lastName, email, password } = req.body;
+
+      if (!firstName || !lastName || !email || !password) {
+        return res.status(400).json({
+          status: false,
+          message: messages.ALL_FIELDS_REQUIRED
+        });
+      }
+
+      const hashPassword = hashSync(password, 10);
+      await userModel.create({
+        firstName,
+        lastName,
+        email,
+        password: hashPassword
+      });
+
+      return res.status(200).json({
+        status: true,
+        message: messages.SINGUP_SUCCESS
+      });
+    } catch (error) {
+      console.error("signup(): catch(): error : ", error);
+      return res.status(200).json({
+        status: false,
+        message: messages.INTERNAL_SERVER_ERROR
+      });
+    }
+  }
 };
--- controllers/productController.js
+++ controllers/productController.js
@@ -30,28 +30,124 @@ module.exports = {
       });
     }
   },
-  getAllProducts: (req, res) => {
-    return res.status(200).json({
-      success: true,
-      message: "Data found"
-    });
+  getAllProducts: async (req, res) => {
+    try {
+      const { page = 1, limit = 10, orderBy = "createdAt", orderDir = "DESC" } = req.query;
+
+      const pageNum = parseInt(page);
+      const limitNum = parseInt(limit);
+
+      // Define sorting order, convert orderDir correctly (1 for ASC, -1 for DESC)
+      const sort = {};
+      sort[orderBy] = orderDir.toUpperCase() === "DESC" ? -1 : 1;
+
+      // Fetch paginated data with sorting
+      const products = await productModel.find()
+        .sort(sort)
+        .limit(limitNum)
+        .skip((pageNum - 1) * limitNum);
+
+      // Count total documents
+      const totalProducts = await productModel.countDocuments();
+
+      // Calculate total pages
+      const totalPages = Math.ceil(totalProducts / limitNum);
+
+      // Return the response
+      return res.status(200).json({
+        success: true,
+        message: messages.DATA_FOUND,
+        data: {
+          totalProducts,
+          totalPages,
+          currentPage: pageNum,
+          products
+        }
+      });
+    } catch (error) {
+      console.error("getAllProducts(): catch(): error : ", error);
+      return res.status(500).json({
+        success: false,
+        message: messages.INTERNAL_SERVER_ERROR
+      });
+    }
   },
-  getProductById: (req, res) => {
-    return res.status(200).json({
-      success: true,
-      message: "Data found"
-    });
+  getProductById: async (req, res) => {
+    try {
+      const productId = req.params.id;
+
+      const product = await productModel.findById(productId);
+
+      return res.status(200).json({
+        success: true,
+        message: messages.DATA_FOUND,
+        data: product
+      });
+    } catch (error) {
+      console.error("getProductById(): catch(): error : ", error);
+      return res.status(500).json({
+        success: false,
+        message: messages.INTERNAL_SERVER_ERROR
+      });
+    }
   },
-  updateProduct: (req, res) => {
-    return res.status(200).json({
-      success: true,
-      message: "Updated successfully"
-    });
+  updateProduct: async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const { title, description } = req.body;
+
+      if (!title || !description) {
+        return res.status(400).json({
+          success: false,
+          message: messages.ALL_FIELDS_REQUIRED
+        });
+      }
+
+      const updatedProduct = await productModel.findByIdAndUpdate(productId, {
+        title,
+        description
+      });
+      if (!updatedProduct) {
+        return res.status(400).json({
+          success: false,
+          message: messages.DATA_NOT_FOUND
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: messages.UPDATE_SUCCESS
+      });
+    } catch (error) {
+      console.error("getProductById(): catch(): error : ", error);
+      return res.status(500).json({
+        success: false,
+        message: messages.INTERNAL_SERVER_ERROR
+      });
+    }
   },
-  deleteProduct: (req, res) => {
-    return res.status(200).json({
-      success: true,
-      message: "Deleted successfully"
-    });
+  deleteProduct: async (req, res) => {
+    try {
+      const productId = req.params.id;
+
+      const deletedProduct = await productModel.findByIdAndDelete(productId);
+      if (!deletedProduct) {
+        return res.status(400).json({
+          success: false,
+          message: messages.DATA_NOT_FOUND
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: messages.DELETE_SUCCESS
+      });
+    } catch (error) {
+      console.error("getProductById(): catch(): error : ", error);
+      return res.status(500).json({
+        success: false,
+        message: messages.INTERNAL_SERVER_ERROR
+      });
+    }
   }
 };
